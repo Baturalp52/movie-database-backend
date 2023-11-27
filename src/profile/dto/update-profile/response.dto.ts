39fa6851639ba6756cec7ff721bba2f67ead657a
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2023-11-27 19:41:29 +0300

    Add data property to PutProfileResponseDto

--- src/profile/dto/update-profile/response.dto.ts
+++ src/profile/dto/update-profile/response.dto.ts
@@ -1,3 +1,11 @@
 import { ResponseDto } from 'src/core/dto/response.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsOptional } from 'class-validator';
 
-export class PutProfileResponseDto extends ResponseDto {}
+export class PutProfileResponseDto extends ResponseDto {
+  @ApiProperty()
+  @Type()
+  @IsOptional()
+  readonly data: any;
+}
